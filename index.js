98a79f672b7261f9f83a297adc3773605a345328
Iveel2010 <Iweel20101216@gmail.com>
2024-08-14 18:14:22 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+// const email = document.getElementById("email");
+// const password = document.getElementById("password");
+// const number = document.getElementById("number");
+// const button = document.getElementsByClassName("btn btn-one")[0];
+// const isNumeric = (string) => /^[+-]?\d+(\.\d+)?$/.test(string);
+// function emaila() {
+//   const aemail = email.value;
+//   if (aemail.includes("@")) {
+//     alert("ok");
+//   } else {
+//     alert("@ bagui bn");
+//   }
+// }
+// function isNumber() {
+//   const number1 = number.value;
+//   if (number1.length >= 8) {
+//     alert("number bolj bn");
+//   }
+//   if (number1.length < 8) {
+//     alert("number bolhgui bn");
+//   }
+//   if (isNumeric(number.value)) {
+//     alert("number tootai bn");
+//   }
+// }
+// function isPassword() {
+//   const password1 = password.value;
+//   if (password1.length >= 8) {
+//     alert("password bolj bn");
+//   }
+//   if (password1.length < 8) {
+//     alert("password bolhgui bn");
+//   }
+//   if (isNumeric(password.value)) {
+//     alert("password tootai bn");
+//   }
+// }
+// button.addEventListener("click", function () {
+//   emaila();
+//   isNumber();
+//   isPassword();
+// });
+console.log(parseInt("123")); // 123
+console.log(parseInt("aaspdiaspdaosdiasdpaisdoajsoq7328723bc")); // 123
